e897ed2f3da37daf58a7b7071b1c8d4c1fee47c0
MdHasanRaju <mdhasanraju2017@gmail.com>
2021-11-11 15:11:24 +0600

    orders added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
+const  ObjectId = require('mongodb').ObjectId;
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -10,13 +11,53 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.6cjag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// user : carsDB
+// pass: U7GlURVw15Fq6yJg
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6cjag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri)
+async function run() {
+  try{
+    await client.connect();
+    const database = client.db("cars_shop");
+    const carsCollection = database.collection("cars");
+    const ordersCollection = database.collection("orders");
+
+    // all cars
+    app.get('/cars', async(req, res) => {
+      const cursor = carsCollection.find({});
+      const result = await cursor.toArray();
+      // console.log(result)
+      res.send(result);
+    })
+
+    // single car
+    app.get('/singleCar/:id', async(req, res) => {
+      const id = req.params.id;
+      const Object = {_id: ObjectId(id)};
+      const result = await carsCollection.find(Object).toArray();
+      res.send(result);
+    })
+
+      // add orders to the database
+      app.post('/addOrders', async(req, res) => {
+        const orders = req.body;
+        const result = await ordersCollection.insertOne(orders)
+        console.log(result);
+        res.send(result)
+      })
+
+  }
+  finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("hello cars world!");
 });
 
 app.listen(port, () => {
